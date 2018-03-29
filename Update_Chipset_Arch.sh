18cf1ca3f864260fb8844234b0ae41944f158707
Hallegraeff <tim.hallegraeff@tno.nl>
2018-03-29 08:56:10 +0200

    new sources

--- Update_Chipset_Arch.sh
+++ Update_Chipset_Arch.sh
@@ -3,12 +3,20 @@
 # Raspbian comes with armv6 only, newer raspberry pi's
 
 # before running this script edit /etc/apt/sources.list to:
+
+# these are the first ones that currently do not work as they say
 #deb http://merlin.fit.vutbr.cz/debian/ jessie main contrib non-free
 #deb-src http://merlin.fit.vutbr.cz/debian/ jessie main
 
 #deb http://security.debian.org/ jessie/updates main
 
 #deb http://merlin.fit.vutbr.cz/debian/ jessie-updates main
+
+# try this one:
+# deb http://mirrors.acm.jhu.edu/debian/ jessie main contrib non-free
+# deb http://mirrordirector.raspbian.org/raspbian/ jessie rpi
+# the last one is commented out
+#deb http://mirrordirector.raspbian.org/raspbian/ jessie main contrib non-free rpi
 
 apt-get update
 apt-get install debian-archive-keyring
