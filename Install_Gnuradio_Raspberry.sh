a57bdff89f531fe07f82631f3518becaad1db400
Hallegraeff <tim.hallegraeff@tno.nl>
2018-04-12 13:51:34 +0200

    new

--- Install_Gnuradio_Raspberry.sh
+++ Install_Gnuradio_Raspberry.sh
@@ -1,65 +1,33 @@
 #! /bin/bash
 # First we install needed dependancies.
-# sudo wget -O /usr/bin/zram.sh https://raw.githubusercontent.com/novaspirit/rpi_zram/master/\zram.sh 
 #######
-apt update
-apt upgrade
-# download all files from git
-git clone https://github.com/gnuradio/gnuradio.git
+apt-get install git build-essential cmake libusb-1.0-0-dev
+apt-get install libasound2-dev
+apt-get install python-cheetah libboost-all-dev swig2.0 python-pkgconfig liborc-0.4-dev libfftw3-dev libasound2-dev libzmq-dev libgsl0-dev python-sphinx libcppunit-dev libgsm1-dev python-numpy libgsl0-dev python-mako
+apt-get install doxygen
+apt-get install python-qt4 libqt4-dev python-qwt5-qt4
+apt-get install python-wxgtk3.0
+apt-get update
+apt-get upgrade
+apt-get install gnuradio gnuradio-dev
+
+gnuradio-companion --version
+sleep 30
 
 cd Desktop
 mkdir sdrplay
 cd sdrplay
 git clone https://github.com/willcode/gr-osmosdr
-cd ../..
-
-##########################
-# change architecture of the gcc compiler
-cp /etc/apt/sources.list /etc/apt/sources.bak.list
-echo "deb http://merlin.fit.vutbr.cz/debian/ jessie main contrib non-free
-deb-src http://merlin.fit.vutbr.cz/debian/ jessie main
-
-deb http://security.debian.org/ jessie/updates main
-
-deb http://merlin.fit.vutbr.cz/debian/ jessie-updates main" > /etc/apt/sources.list
-apt update
-apt install --only-upgrade gcc-4.9
-rm /etc/apt/sources.list
-cp /etc/apt/sources.bak.list /etc/apt/sources.list
-apt update
-#
-# build VOLK
-git submodule init
-git submodule update
-cd volk
+cd gr-osmosdr/
 mkdir build
 cd build
-cmake ..
-make
-sudo make install
-cd ../..
-
-# build gnuradio
-mkdir build
-cd build
-cmake -DENABLE_DOXYGEN=OFF ..
-make
-sudo make install
-cd ../..
-
-# build gr-osmosdr
-cd gr-osmosdr
-git checkout sdrplay2
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=~/gr-osmosdr -DENABLE_NONFREE=yes ..
+cmake -DCMAKE_INSTALL_PREFX=~/gr-osmosdr -DENABLE_NONFREE=yes ..
 make
 mkdir ~/gr-osmosdr
 make install
+ldconfig
 
-# set ENV variables
-echo "export LD_LIBRARY_PATH=/home/pi/gr-osmosdr/lib:" >> .bashrc
-echo "export PYTHONPATH=/home/pi/gr-osdmosdr/lib/python2.7/dist-packages:" >> .bashrc
-reboot
+cd ../../..
+
 
 
